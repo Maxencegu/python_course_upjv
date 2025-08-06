8ac14508937f5ca664a72d23a353dfa156beee81
Maxencegu <maxenceguery@yahoo.fr>
2025-08-06 22:01:20 +0200

    Update hidden_tests_00_python_02.py

--- hidden_tests_00_python_02.py
+++ hidden_tests_00_python_02.py
@@ -2,8 +2,8 @@
 
 def hidden_test_exo1(x, y, sum_result):
     errors = []
-    if x != 8 or y != 2:
-        errors.append("x should be 8 and y should be 2.")
+    if x != 4 or y != 6:
+        errors.append("x should be 4 and y should be 6.")
     if sum_result != x + y:
         errors.append("sum_result is incorrect.")
     return "✅ All tests passed for exercise 1!" if not errors else "❌ " + "\n".join(errors)
